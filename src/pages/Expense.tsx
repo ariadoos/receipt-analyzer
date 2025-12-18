b8590ce1e464cefa91c76a753f0d2e1e8f9f72d7
Nikesh Adhikari <neekesh21@gmail.com>
2025-12-18 20:32:49 +0200

    Add expense component

--- src/pages/Expense.tsx
+++ src/pages/Expense.tsx
@@ -1,9 +1,127 @@
+import { ConfirmDialog } from '@/components/common/ConfirmDialog';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import { toast } from 'sonner';
+import { ExpenseForm, type ExpenseFormData } from './ExpenseForm';
+
 const Expense = () => {
+    const [editingId, setEditingId] = useState<number | null>(null);
+    const [expenses, setExpenses] = useState([
+        { description: 'Grocery shopping', amount: 85.50, categoryId: 'Food', date: '12/15', color: 'red' },
+        { description: 'Gas station', amount: 45.00, categoryId: 'Transport', date: '12/14', color: 'blue' },
+        { description: 'Movie tickets', amount: 32.00, categoryId: 'Entertainment', date: '12/13', color: 'purple' },
+        { description: 'Internet bill', amount: 60.00, categoryId: 'Bills', date: '12/12', color: 'green' },
+        { description: 'Restaurant dinner', amount: 78.20, categoryId: 'Food', date: '12/11', color: 'red' }
+    ]);
+
+    const handleCreate = (data: ExpenseFormData) => {
+        console.log(data);
+        toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            action: {
+                label: "Undo",
+                onClick: () => console.log("Undo"),
+            },
+        })
+    }
+
+    // Handle Update
+    const handleUpdate = (data: ExpenseFormData) => {
+        setEditingId(null);
+        toast.success("Expense updated!");
+    };
+
+    const handleDelete = (expenseId: number) => {
+        // setCategories(filteredCategories);
+        toast.success("Expense updated!");
+
+    }
+
     return (
-        <div className="space-y-6">
-            <h1 className="text-3xl font-bold text-foreground">Expense</h1>
-            <p className="text-muted-foreground">Expense</p>
-        </div>
+        <div className="flex flex-col md:flex-row gap-4 justify-between">
+            <Card className="w-full md:max-w-md rounded-md">
+                <CardHeader>
+                    <CardTitle>Add New Expense</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <ExpenseForm id="form-expense" onSubmit={handleCreate} buttonLabel="Save" />
+                </CardContent>
+            </Card>
+
+            <Card className="w-full rounded-md md:max-w-2xl">
+                <CardHeader>
+                    <CardTitle>Recent Expenses</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="space-y-2">
+                        {expenses.map((exp, idx) => (
+                            <AnimatePresence key={idx} mode="wait">
+                                {
+                                    editingId === idx ? (
+                                        <motion.div
+                                            key={`edit-${idx}`}
+                                            initial={{ opacity: 0, scale: 0 }}
+                                            animate={{ opacity: 1, scale: 1 }}
+                                            exit={{ opacity: 0, scale: 0 }}
+                                            transition={{
+                                                duration: 0.3
+                                            }}
+                                            className='overflow-hidden border border-solid border-border rounded p-3'>
+                                            <ExpenseForm id="form-expense-edit"
+                                                initialData={exp}
+                                                onSubmit={handleUpdate}
+                                                onCancel={() => setEditingId(null)}
+                                                buttonLabel="Update" />
+                                        </motion.div>
+                                    ) :
+                                        (<motion.div
+                                            key={`view-${idx}`}
+                                            initial={{ opacity: 0, scale: 0 }}
+                                            animate={{ opacity: 1, scale: 1 }}
+                                            exit={{ opacity: 0, scale: 0 }}
+                                            transition={{
+                                                duration: 0.3
+                                            }}
+                                            className="flex flex-row border border-solid border-border rounded p-3">
+
+                                            <div className="flex flex-col flex-1">
+                                                <div className="font-semibold text-left text-sm">{exp.description}</div>
+                                                <div className="flex gap-2 items-center mt-1">
+                                                    <span className={`flex items-center justify-center text-xs px-2 py-0.5 rounded bg-amber-300 text-amber-50`}>
+                                                        {/* {exp.category} */}Category
+                                                    </span>
+                                                    <span className="text-xs text-muted-foreground">{exp.date}</span>
+                                                </div>
+                                            </div>
+                                            <div className="text-right">
+                                                <div className="font-bold px-3">${exp.amount}</div>
+                                                <div className="text-xs text-muted-foreground cursor-pointer">
+                                                    <Button className="cursor-pointer" variant="ghost" size="sm" onClick={() => setEditingId(idx)}>
+                                                        Edit
+                                                    </Button>
+                                                    <ConfirmDialog
+                                                        title={`Delete ${exp.description}?`}
+                                                        onConfirm={() => handleDelete(idx)}
+                                                        trigger={
+                                                            <Button className="cursor-pointer" variant="ghost" size="sm">
+                                                                Delete
+                                                            </Button>
+                                                        }
+                                                    />
+
+                                                </div>
+                                            </div>
+                                        </motion.div>)
+                                }
+                            </AnimatePresence>
+
+                        ))}
+                    </div>
+                </CardContent>
+            </Card>
+        </div >
     )
 }
 
---
+++ src/pages/ExpenseForm.tsx
@@ -0,0 +1,133 @@
+import { Button } from "@/components/ui/button";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { InputGroup, InputGroupAddon, InputGroupText, InputGroupTextarea } from "@/components/ui/input-group";
+import { CURRENCY } from "@/constants/currency";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import z from 'zod';
+
+interface ExpenseBackend {
+    id: number;
+    amount: number; // Backend sends a number
+    categoryId: string
+    description: string;
+}
+
+type ExpenseFormData = z.infer<typeof formSchema>
+
+interface ExpenseFormProps {
+    id: string;
+    initialData?: ExpenseBackend;
+    onSubmit: (data: ExpenseFormData) => void;
+    onCancel?: () => void;
+    buttonLabel?: string;
+}
+
+const formSchema = z.object({
+    categoryId: z
+        .string()
+        .optional(),
+    amount: z
+        .string()
+        .min(1, "Amount is required")
+        .refine(
+            (val) => !val || !isNaN(Number(val)),
+            "Must be a valid number"
+        ),
+    description: z
+        .string()
+        .min(1, "Description must contain some character.")
+        .max(50, "Description must be at least 50 character.")
+})
+
+const ExpenseForm = ({ id, initialData, onSubmit, onCancel, buttonLabel = "Save" }: ExpenseFormProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData ? {
+            ...initialData,
+            amount: String(initialData.amount) // Ensure number becomes string for input
+        } : {
+            categoryId: "",
+            amount: "",
+            description: ""
+        },
+    });
+
+    return (
+        <form id={id} onSubmit={form.handleSubmit(onSubmit)} className="space-y-4" >
+            <FieldGroup>
+                {/* Amount Field */}
+                <Controller
+                    name="amount"
+                    control={form.control} render={({ field, fieldState }) =>
+                        <Field data-invalid={fieldState.invalid}>
+                            <FieldLabel htmlFor={`${id}-amount`}>
+                                Amount {CURRENCY}
+                            </FieldLabel>
+                            <Input
+                                {...field}
+                                id={`${id}-amount`}
+                                aria-invalid={fieldState.invalid}
+                                placeholder={`20`}
+                                autoComplete="off"
+                            />
+                            {fieldState.invalid && (
+                                <FieldError className='text-left' errors={[fieldState.error]} />
+                            )}
+                        </Field>
+                    }
+                />
+
+                {/* Description Field */}
+                <Controller
+                    name="description"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field data-invalid={fieldState.invalid}>
+                            <FieldLabel htmlFor={`${id}-description`}>
+                                Description
+                            </FieldLabel>
+                            <InputGroup>
+                                <InputGroupTextarea
+                                    {...field}
+                                    id={`${id}-description`}
+                                    placeholder="Groceries"
+                                    className="resize-none"
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                <InputGroupAddon align="block-end">
+                                    <InputGroupText className="tabular-nums">
+                                        {field.value?.length ?? 0}/50 characters
+                                    </InputGroupText>
+                                </InputGroupAddon>
+                            </InputGroup>
+                            {fieldState.invalid && (
+                                <FieldError className='text-left' errors={[fieldState.error]} />
+                            )}
+                        </Field>
+                    )}
+                />
+            </FieldGroup>
+
+            <div className="flex justify-end gap-2 pt-2">
+                <Field orientation="horizontal">
+                    {onCancel && (
+                        <Button className="cursor-pointer" type="button" variant="outline" onClick={onCancel}>
+                            Cancel
+                        </Button>
+                    )}
+                    {!onCancel && (
+                        <Button className="cursor-pointer" type="button" variant="outline" onClick={() => form.reset()}>
+                            Reset
+                        </Button>
+                    )}
+                    <Button className="cursor-pointer" type="submit" form={id}>{buttonLabel}</Button>
+                </Field>
+            </div>
+        </form >
+    );
+};
+
+export { ExpenseForm, type ExpenseFormData };
+
